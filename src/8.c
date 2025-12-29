0a4224c8fbd9cc03308feb0dc1c7a65d5eff96da
Purple147 <gorcraft1380@gmail.com>
2025-12-29 23:54:58 +0330

    learning: #defining and '''

---
+++ src/8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    int Mohammad, Hacker;
+    int Counter;
+
+
+    for (Mohammad = 25, Hacker = 30, Counter = 0; Mohammad <= Hacker; Mohammad += 1, Counter += 1) {
+        printf("for mohammad to being a good hacker %d years left\n", Counter);
+    }
+}
